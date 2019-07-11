41bb5d7d4df9e761ec765f9e951b57f0254af418
Jelisha <jelisha.delacruz@gmail.com>
2019-07-12 01:11:34 +1000

    First commit

---
+++ Assets/Scripts/SnekControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnekControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject snekPrefab;
+    private List<GameObject> snek = new List<GameObject>();
+    void Start()
+    {
+        snek.Add(Instantiate(snekPrefab));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
